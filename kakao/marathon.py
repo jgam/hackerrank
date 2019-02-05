4dba110bb578e9894adaf4e6c4737a27d657e23c
Jeonghan Gam <jgam@nd.edu>
2019-02-05 10:39:45 +0900

    done

--- kakao/marathon.py
+++ kakao/marathon.py
@@ -2,13 +2,13 @@ import time
 
 def solution(participant, completion):
     answer = ''
-    '''
     for i in completion:
         if i in participant:
             participant.remove(i)
     return participant[0]
-    '''
+    
     #this taking too much time
+    '''
     participant = sorted(participant)
     completion = sorted(completion)
     for i in range(len(participant)-1):
@@ -17,7 +17,7 @@ def solution(participant, completion):
         else:
             return participant[i]
     return participant[-1]
-
+    '''
 participant = ['marina', 'josipa', 'nikola', 'vinko', 'filipa']
 completion = ['josipa', 'filipa', 'marina', 'nikola']
 
@@ -28,6 +28,10 @@ print(t2-t1)
 
 #better records
 '''
+6.604194641113281e-05
+7.390975952148438e-05
+7.009506225585938e-05
+6.008148193359375e-05
 '''
 
 #original records
