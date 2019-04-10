fca0f3f9e9c7ad6e67da48a705ff5f7bd4fb03b3
jgam <jgam@alumni.nd.edu>
2019-04-10 10:52:40 +0900

    done

---
+++ sorting/pairs.py
@@ -0,0 +1,10 @@
+def pairs(k, arr):
+    numbers = set()
+    count = 0
+    for n in a:
+        if n + k in numbers:
+            count += 1
+        if n - k in numbers:
+            count += 1
+        numbers.add(n)
+    return count
